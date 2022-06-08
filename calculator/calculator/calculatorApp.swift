66fce806e60bacf0851dc11b3dd038e7f940be58
Mohcine Amzyl <67154390+AmzylMohcine@users.noreply.github.com>
2022-06-08 14:11:10 +0200

    Add files via upload

---
+++ calculator/calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Utilisateur invité on 08/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
